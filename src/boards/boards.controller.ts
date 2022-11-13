5b0f2a02c80e93c2fe5caf388813a876ca543ff5
jinuklee <20181680@edu.hanbat.ac.kr>
2022-11-13 15:06:50 +0900

    게시물 생성 메서드

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { BoardsService } from "./boards.service";
+import { Board } from "./boards.model";
 
 @Controller("boards")
 export class BoardsController {
-  constructor(private boardsService: BoardsService) {}
+  constructor(private boardsService: BoardsService) {
+  }
 
   @Get('/')
-  getAllBoard() {
+  getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
 
   }
-
-
+  @Post()
+  createBoard(
+    @Body('title') title:string,
+    @Body('dscription') description :string
+  ):Board {
+    return this.boardsService.createBoard(title, description)
 }
+}
+
+
