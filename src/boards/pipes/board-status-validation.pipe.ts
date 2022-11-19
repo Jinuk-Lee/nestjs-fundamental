8b6f66fd93961667c15525cee52bcafd5f291b2f
jinuklee <20181680@edu.hanbat.ac.kr>
2022-11-19 17:28:17 +0900

    readonly

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,6 +1,12 @@
 import {ArgumentMetadata, PipeTransform} from "@nestjs/common";
+import {BoardsStatus} from "../boards.model";
 
 export class BoardStatusValidationPipe implements PipeTransform{
+    readonly StatusOptions = [
+        BoardsStatus.PRIVATE
+    ]
+
+
     transform(value: any, metadata: ArgumentMetadata){
         console.log('value',value);
         console.log('metadata',metadata);
