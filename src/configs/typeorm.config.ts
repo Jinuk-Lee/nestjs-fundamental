566e5390a1d2877f2fa6f790e3beb80d4a2b2b08
jinuklee <20181680@edu.hanbat.ac.kr>
2022-11-24 00:09:42 +0900

    typeORM 설정파일 작성

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,6 +1,6 @@
 import {TypeOrmModuleOptions} from "@nestjs/typeorm";
 
-export const typeRMconfig : TypeOrmModuleOptions = {
+export const typeORMConfig : TypeOrmModuleOptions = {
     type : 'postgres',
     host : 'localhost',
     port: 5432,
