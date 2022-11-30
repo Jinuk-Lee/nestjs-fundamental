edc340f85001d65e3d73c9bf0e74dfd626a7d758
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-01 03:15:11 +0900

    Repository Pattern 적용

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,9 +1,22 @@
-import {EntityRepository, Repository} from "typeorm";
+import {Repository} from "typeorm";
 import {Board} from "./board.entity";
+import {CreateBoardDto} from "./dto/create-board.dto";
+import {BoardsStatus} from "./board-status.enum";
 
-@EntityRepository(Board)
+
 export class BoardRepository extends Repository<Board> {
- constructor() {
-     
- }
+
+    async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+        const {title, description} = createBoardDto;
+
+        const board = this.create({
+            title,
+            description,
+            status: BoardsStatus.PUBLIC
+        })
+
+        await this.save(board);
+        return board;
+
+    }
 }
