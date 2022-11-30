69fe3d8a57d26831e3b2c97ccaf41ab8e725475e
jinuklee <20181680@edu.hanbat.ac.kr>
2022-11-30 23:37:29 +0900

    @Column() 추가

--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -7,10 +7,10 @@ export class Board extends BaseEntity {
     @PrimaryGeneratedColumn() //id 열이 기본키 열임을 나타낸다.
     id : number;
 
-    @Column()
+    @Column() //@Column()은 기본키가 아닌 열
     title:string;
 
-    @Column
+    @Column()
     description : string;
 
     @Column()
