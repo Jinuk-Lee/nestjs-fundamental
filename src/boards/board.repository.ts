4ddd35c6d606c51de9738cd1f2436919988686ec
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-02 01:18:47 +0900

    오류 해결을 위한 코드 수정

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,22 +1,7 @@
 import {Repository} from "typeorm";
 import {Board} from "./board.entity";
-import {CreateBoardDto} from "./dto/create-board.dto";
-import {BoardsStatus} from "./board-status.enum";
 
 
 export class BoardRepository extends Repository<Board> {
 
-    async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
-        const {title, description} = createBoardDto;
-
-        const board = this.create({
-            title,
-            description,
-            status: BoardsStatus.PUBLIC
-        })
-
-        await this.save(board);
-        return board;
-
-    }
 }
