ff29de689d21ae4b53e7c71424c9e252ed73fad3
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-02 01:33:53 +0900

    오류 해결을 위한 코드 수정

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,5 @@
 import {Injectable, NotFoundException} from "@nestjs/common";
-import {BoardsStatus} from "./board-status.enum";
+import {BoardStatus} from "./board-status.enum";
 import {CreateBoardDto} from "./dto/create-board.dto";
 import {InjectRepository} from "@nestjs/typeorm";
 import {BoardRepository} from "./board.repository";
@@ -9,43 +9,29 @@ import {Board} from "./board.entity";
 @Injectable()
 export class BoardsService {
     constructor(
-        @InjectRepository(BoardRepository)
+        /**
+         * @InjectRepository를 이용해 종속성 주입
+         * 서비스에서 Repository를 이용한다고 알림
+         */
+        @InjectRepository(Board)
         private boardRepository: BoardRepository
     ) {
     }
 
-    // //boards의 형식을 Board타입으로 설정,여러값이기 때문에 Board[]
-    //
-    // getAllBoards(): Board[] {
-    //     return this.boards;  //boards배열의 모든 값을 가져옴.
-    // }
-    //
-    // createBoard(createBoardDto: CreateBoardDto) { //게시물 생성
-    //     const {title, description} = createBoardDto;
-    //     const board: Board = {
-    //         id: uuid(),
-    //         title,
-    //         description,
-    //         status: BoardsStatus.PUBLIC
-    //     };
-    //     this.boards.push(board);
-    //     return board;
-    // }
-    //
-    createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
-        return this.boardRepository.createBoard(createBoardDto);
+    async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+        const { title, description } = createBoardDto;
+
+        const board = this.boardRepository.create({
+            title,
+            description,
+            status: BoardStatus.PUBLIC,
+        });
+
+        await this.boardRepository.save(createBoardDto);
+        return board;
     }
 
-    // async getBoardById(id: number): Promise<Board> {
-    //     const found = await this.boardRepository.findOne(id);
-    //     //constructor에 boardRepository를 주입했기 때문에 사용이 가능해짐.
-    //
-    //     if (!found) {
-    //         throw new NotFoundException(`Can't find Board with id ${id}`);
-    //     }
-    //
-    //     return found;
-    // }
+
     async getBoardById(id: number): Promise<Board> {
       const found = await this.boardRepository.findOne({ where: { id: id } });
 
@@ -54,16 +40,7 @@ export class BoardsService {
       }
       return found;
     }
-
-    // getBoardById(id: string): Board {
-    //     const found = this.boards.find((board) => board.id === id);
-    //
-    //     if (!found) {
-    //         throw new NotFoundException(`Can't find Bord with id ${id}`);
-    //     }
-    //     return found;
-    // }
-    //
+    
     async deleteBoard(id: number): Promise<void> {
         const result = await this.boardRepository.delete(id);
 
