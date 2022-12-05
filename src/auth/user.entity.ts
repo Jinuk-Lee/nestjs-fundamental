98a7b8b0e045b4e79d360f5cca17ffc305d95263
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-05 18:14:41 +0900

    User을 위한 Entity,Repository 생성

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,14 @@
+import {BaseEntity, Column, Entity} from "typeorm";
+import {PrimaryGeneratedColumn} from "typeorm/browser";
+
+@Entity
+export class User extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    password:string;
+}
