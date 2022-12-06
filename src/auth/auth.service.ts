2ab639fcff30d94a84844ff6752ba6af3cd7ee28
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-06 22:28:35 +0900

    인증기능 구현을 위한 준비

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "./user.entity";
+import {UserRepository} from "./user.repository";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        /**
+         * @InjectRepository 를 이용해 종속성 주입
+         * 서비스에서 Repository 를 이용한다고 알림
+         */
+        @InjectRepository(User)
+        private useRepository:UserRepository
+    ) { }
+}
