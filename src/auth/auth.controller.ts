06540830e654556d13e1df52b7bc7e904da25379
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-07 21:37:21 +0900

    데코레이터와 Module import로 오류 해결

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import {Body, Controller, Post} from '@nestjs/common';
 import {AuthService} from "./auth.service";
 import {AuthCredentialsDto} from "./dto/auth-credential.dto";
+import {User} from "./user.entity";
 
 @Controller('auth')
 export class AuthController {
@@ -8,7 +9,7 @@ export class AuthController {
     }
 
     @Post('/signup')
-    signUp(@Body() authcredentialsDto: AuthCredentialsDto): Promise<void> {
+    signUp(@Body() authcredentialsDto: AuthCredentialsDto): Promise<User> {
         return this.authService.signUp(authcredentialsDto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,11 +11,18 @@ export class AuthService {
          * @InjectRepository 를 이용해 종속성 주입
          * 서비스에서 Repository 를 이용한다고 알림
          */
-        @InjectRepository(UserRepository)
+        @InjectRepository(User)
         private userRepository:UserRepository
     ) { }
-    async signUp(authCredentialsDto : AuthCredentialsDto):Promise<void>{
-        return this.userRepository.createUser(authCredentialsDto);
-    }
 
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+        const {username, password} = authCredentialsDto;
+        const user = this.userRepository.create({
+            username,
+            password
+        });
+
+        await this.userRepository.save(user);
+        return user;
+    }
 }
