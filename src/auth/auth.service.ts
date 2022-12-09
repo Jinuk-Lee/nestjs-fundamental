13bb1d5de26a5e2673c44c82ae6b67f14d80c5e0
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-09 23:13:58 +0900

    유저 중복 생성 예외처리

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {ConflictException, Injectable, InternalServerErrorException} from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
 import {User} from "./user.entity";
 import {UserRepository} from "./user.repository";
@@ -12,8 +12,9 @@ export class AuthService {
          * 서비스에서 Repository 를 이용한다고 알림
          */
         @InjectRepository(User)
-        private userRepository:UserRepository
-    ) { }
+        private userRepository: UserRepository
+    ) {
+    }
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
         const {username, password} = authCredentialsDto;
@@ -21,8 +22,16 @@ export class AuthService {
             username,
             password
         });
-
-        await this.userRepository.save(user);
+        try {
+            await this.userRepository.save(user);
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Existing username');
+            } else {
+                throw new InternalServerErrorException();
+            }
+            console.log('error', error);
+        }
         return user;
     }
 }
