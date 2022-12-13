d3666236e5c88518bb487e76d9846642dd73d42f
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-13 19:48:15 +0900

    JWT,Passport 모듈 등록

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import {Module} from '@nestjs/common';
+import {AuthController} from './auth.controller';
+import {AuthService} from './auth.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {User} from "./user.entity";
+import {JwtModule} from "@nestjs/jwt";
+import {PassportModule} from "@nestjs/passport";
 
 @Module({
-  imports :[
-      TypeOrmModule.forFeature([User])
-  ],
-  controllers: [AuthController],
-  providers: [AuthService]
+    imports: [
+        PassportModule.register({defaultStrategy: 'jwt'}),
+        JwtModule.register({
+            secret: 'Secret1234',
+            signOptions: {
+                expiresIn: 60 * 60,
+            }
+        }),
+        TypeOrmModule.forFeature([User])
+    ],
+    controllers: [AuthController],
+    providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule {
+}
