b30e4143346cac314234c06018fd6c3d3ed5068d
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-13 15:43:26 +0900

    비밀번호 암호화 소스 코드 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,8 @@ import {InjectRepository} from "@nestjs/typeorm";
 import {User} from "./user.entity";
 import {UserRepository} from "./user.repository";
 import {AuthCredentialsDto} from "./dto/auth-credential.dto";
-import * as bcrypt from 'bcryptjs';
+import * as bcrypt from "bcryptjs";
+
 @Injectable()
 export class AuthService {
     constructor(
@@ -18,7 +19,9 @@ export class AuthService {
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
         const {username, password} = authCredentialsDto;
-        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
         const user = this.userRepository.create({
             username,
             password: hashedPassword
