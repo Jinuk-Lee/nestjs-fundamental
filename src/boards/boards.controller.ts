3abd90a26cab939fe8c64d64e816159e1fe70f31
jinuklee <20181680@edu.hanbat.ac.kr>
2022-12-18 17:18:26 +0900

    에러확인을 위한 로그

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,17 +1,17 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post, UseGuards,
-  UsePipes,
-  ValidationPipe
+    Body,
+    Controller,
+    Delete,
+    Get, Logger,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post, UseGuards,
+    UsePipes,
+    ValidationPipe
 } from "@nestjs/common";
-import { BoardsService } from "./boards.service";
-import { CreateBoardDto } from "./dto/create-board.dto";
+import {BoardsService} from "./boards.service";
+import {CreateBoardDto} from "./dto/create-board.dto";
 import {Board} from "./board.entity";
 import {BoardStatusValidationPipe} from "./pipes/board-status-validation.pipe";
 import {BoardStatus} from "./board-status.enum";
@@ -22,40 +22,46 @@ import {GetUser} from "../auth/get-user.decorator";
 @Controller("boards")
 @UseGuards(AuthGuard())
 export class BoardsController {
-  constructor(private boardsService: BoardsService) {
-  }
-  @Get('/:id')
-  getBoardById(@Param ('id') id :number) : Promise <Board>{
-    return this.boardsService.getBoardById(id);
+    private logger = new Logger(`BoardsController`);
+    constructor(private boardsService: BoardsService) {
+    }
 
-  }
-  @Get()
-  getAllBoard(
-      @GetUser() user:User
-  ): Promise<Board[]>{
-    return this.boardsService.getAllBoards(user);
-  }
-  @Post()
-  @UsePipes(ValidationPipe)
-  createBoard(
-  @Body() createBoardDto: CreateBoardDto,
-  @GetUser() user: User,
-  ): Promise<Board>{
-    return this.boardsService.createBoard(createBoardDto, user);
-  }
+    @Get('/:id')
+    getBoardById(@Param('id') id: number): Promise<Board> {
+        return this.boardsService.getBoardById(id);
 
-  @Delete('/:id')
-  deleteBoard(@Param('id',ParseIntPipe)id ):Promise<void>{
-    return this.boardsService.deleteBoard(id);
-  }
+    }
 
-  @Patch('/:id/status')
-  updateBoardStatus(
-      @Param('id',ParseIntPipe) id:number,
-      @Body('status', BoardStatusValidationPipe) status : BoardStatus,
-      ): Promise<Board>{
-    return this.boardsService.updateBoardStatus(id, status);
-  }
+    @Get()
+    getAllBoard(
+        @GetUser() user: User
+    ): Promise<Board[]> {
+        this.logger.verbose(`User${user.username} trying to get all boards`);
+        return this.boardsService.getAllBoards(user);
+    }
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto,
+        @GetUser() user: User,
+    ): Promise<Board> {
+        return this.boardsService.createBoard(createBoardDto, user);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id', ParseIntPipe) id: number,
+                @GetUser() user: User): Promise<void> {
+        return this.boardsService.deleteBoard(id, user);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+    ): Promise<Board> {
+        return this.boardsService.updateBoardStatus(id, status);
+    }
 
 }
 
